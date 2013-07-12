372fda04a1ecd9bb3b3677b82d1b88498c23171e
Elvio Toccalino <etoccalino@fi.uba.ar>
2013-07-12 01:05:33 -0300

    refactored item_orders view to a simple View

--- base/views.py
+++ base/views.py
@@ -1,28 +1,19 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic.base import View
-from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView
 
 from models import Consumer, ItemOrder
 from forms import NewItemOrderForm, EditItemOrderForm
 
 
-class ItemOrders (ListView):
-    template_name = 'base/item_orders.html'
-    context_object_name = 'item_orders_list'
-
-    def get_queryset(self):
-        consumer = Consumer.objects.get(pk=self.request.user.pk)
-        return consumer.package.item_orders.all()
-
-    def get_context_data(self, **kwargs):
-        context = super(ItemOrders, self).get_context_data(**kwargs)
-        consumer = Consumer.objects.get(pk=self.request.user.pk)
-        context['consumer'] = consumer
-        return context
+class ItemOrders (View):
+    def get(self, request):
+        return render(request, 'base/item_orders.html', {
+            'consumer': Consumer.objects.get(pk=self.request.user.pk)
+        })
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
@@ -61,7 +52,6 @@ class EditItemOrder (UpdateView):
 
 
 class RemoveItemOrder (View):
-
     def post(self, request, pk):
         item = get_object_or_404(ItemOrder, pk=pk)
         consumer = Consumer.objects.get(pk=self.request.user.pk)
